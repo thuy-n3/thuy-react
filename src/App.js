29e092cb1ff8300ae186ee8e37b4e4e3f2762105
thuy-n3 <ngtan3@gmail.com>
2019-02-11 17:36:24 -0600

    initial and images

--- src/App.js
+++ src/App.js
@@ -1,28 +1,21 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Header from './components/header.js';
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <h1>This is from app.js</h1>
+
+      <Header />
+
       </div>
     );
   }
 }
 
 export default App;
+
+
+//<img src={logo} className="App-logo" alt="logo" />
---
+++ src/components/aboutme.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class AboutMe extends Component{
+  render() {
+    return(
+      <div className="aboutMeContainer">
+        
+      </div>
+    )
+  }
+}
