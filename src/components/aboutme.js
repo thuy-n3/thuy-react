dfe51ddb6cb7c9cb85852dc6896cca5c35149d0e
thuy-n3 <ngtan3@gmail.com>
2019-02-12 00:25:10 -0600

    add project and gallery structure

--- src/components/aboutme.js
+++ src/components/aboutme.js
@@ -4,7 +4,10 @@ class AboutMe extends Component{
   render() {
     return(
       <div className="aboutMeContainer">
-        
+        <div className="title"></div>
+        <div className="bioImage"></div>
+        <div className="bioText"></div>
+        <div className="buttonContainer"></div>
       </div>
     )
   }
--- src/components/project.js
+++ src/components/project.js
@@ -1,1 +1,16 @@
 import React, { Component } from 'react';
+
+class Project extends Component{
+  render (){
+    return(
+      <div className="projectContainer">
+        <h1 className="projectTitle"></h1>
+        <h2 className="projectSubTitle"></h2>
+        <img src="/" className="projectImage" />
+        <h3 className="projectText"></h3>
+        <div className="linkContainer"></div>
+        <div className="techUsed"></div>
+      </div>
+    )
+  }
+}
