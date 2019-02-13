8e192b467b4141427cc674dc623f4cfe427d5fcf
thuy-n3 <ngtan3@gmail.com>
2019-02-13 16:52:21 -0600

    contact me social

--- src/components/contactme.js
+++ src/components/contactme.js
@@ -4,7 +4,17 @@ class ContactMe extends Component {
   render(){
     return(
       <div className="contactMeContainer">
-        <div className=""></div>
+        <form className="contactForm">
+          <input className="name" type="text" placeholder="Your Name"></input>
+          <input className="email" type="email" placeholder="Your Email"></input>
+          <textarea className="message" placeholder="message"></textarea>
+          <input className="submit" type="submit" value="Submit"></input>
+        </form>
+        <div className="socialContainer">
+          <a href="/" className="twitter">Twitter</a>
+          <a href="/" className="linkedin">Linkedin</a>
+          <a href="/" className="github">Github</a>
+        </div>
       </div>
     )
   }
